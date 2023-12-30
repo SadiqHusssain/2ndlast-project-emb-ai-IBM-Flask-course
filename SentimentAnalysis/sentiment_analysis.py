851823886d669aba8b43633e89b553c750eead9b
SadiqHusssain <sadiqhussain17647@gmail.com>
2023-12-30 14:55:36 -0500

    Add SentimentAnalysis module

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_json = json.loads(response.text)
+    label = formatted_json['documentSentiment']['label']
+    score = formatted_json['documentSentiment']['score']
+    return {'label: ':label, 'score: ':score}
