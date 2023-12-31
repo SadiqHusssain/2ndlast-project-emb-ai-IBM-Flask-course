6fe02de17b17b08a98b26070e82d6a85f4359ef3
SadiqHusssain <sadiqhussain17647@gmail.com>
2023-12-31 15:47:16 -0500

    did static code analysis for quotation marks

--- server.py
+++ server.py
@@ -5,15 +5,15 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    text_to_analyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get("textToAnalyze")
     result = sentiment_analyzer(text_to_analyze)
-    if result['label'] is None:
+    if result["label"] is None:
         return "Invalid input ! Try Agian"
     return f"The given text has been identified as {result['label'].split('_')[1]} with a score of {result['score']}."
 
 @app.route("/")
 def render_index_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
