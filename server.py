b2dad71e202987a12f7b5017f9fd742b15cc16b3
SadiqHusssain <sadiqhussain17647@gmail.com>
2023-12-31 14:51:02 -0500

    update serve.py modified sent_analyzer() and added code for render_inder_page() and code for deploying the app on specified port and host.

--- server.py
+++ server.py
@@ -1,29 +1,18 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
 from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app :
 app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    result = sentiment_analyzer(text_to_analyze)
+    print(result)
+    return f"The given text has been identified as {result['label'].split('_')[1]} with a score of {result['score']}."
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    app.run(host='0.0.0.0', port=5000, debug=True)
